bf6041c150902525a91465519c97da9e65cf3feb
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-11-30 06:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-//your JS code here. If required.
 const button = document.querySelector("button");
 const weatherData = document.querySelector("#weatherData");
 
-button.addEventListener("click", async function() {
-	const res= await fetch("https://api.openweathermap.org/data/2.5/weather?q=london&appid=fa8b6b96a01db77ae3d253c0e1c9b50a");
-	const resJSON= await res.json();
-	console.log(resJSON.weather[0].main);
-	weatherData.textContent= "Current weather in London: " + resJSON.weather[0].main;
-})
+button.addEventListener("click", async function () {
+    const apiUrl =
+        "https://api.openweathermap.org/data/2.5/weather?q=london&appid=fa8b6b96a01db77ae3d253c0e1c9b50a";
+
+    // Make the API request
+    try {
+        const res = await fetch(apiUrl);
+        const resJSON = await res.json();
+
+        // Display weather information
+        weatherData.textContent =
+            "Current weather in London: " + resJSON.weather[0].main;
+    } catch (error) {
+        console.error("Failed to fetch weather data:", error);
+        weatherData.textContent = "Failed to fetch weather data.";
+    }
+});
